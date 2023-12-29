b272e5bb6ef4ba56d78897c024d26491c51249ba
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2023-12-28 23:44:18 -0600

    Se crean modelos de DB para driver y team

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -4,9 +4,71 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Name required'
+        }
+      }
     },
-  });
+
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Lastname required'
+        }
+      }
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Description required'
+        }
+      }
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "https://img.freepik.com/fotos-premium/hermoso-piloto-carreras-traje-carreras-ilustracion-imagenes-predisenadas-acuarela_962764-33747.jpg",
+      validate: {
+        isUrl: {
+          msg: 'URL required'
+        }
+      }
+    },
+
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate : {
+        notEmpty: {
+          msg: 'Nationality required'
+        }
+      }
+    },
+
+    dateofbirth: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Date of birth requiered'
+        }
+      }
+    }
+  }, { timestamps: false });
 };
---
+++ server/src/models/Team.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Team', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: 'Name required'
+                }
+            }
+        }
+    }, { timestamps: false }); 
+}
